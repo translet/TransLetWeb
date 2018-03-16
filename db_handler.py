fe4f1ad948448c07940de5eb34f8bee12ff660a8
saurabh shukla <saurabhsshukla@gmail.com>
2018-03-15 23:59:32 -0400

    Fetch userdata implemented

--- db_handler.py
+++ db_handler.py
@@ -1,2 +1,34 @@
 import MySQLdb
 import logging
+from app_globals import *
+
+logging.basicConfig(format="%(asctime)s %(funcName)s %(message)s",
+                     filename=LOGDIR+"{0}.log".format(__name__),
+                     level='DEBUG')
+logger = logging.getLogger(__name__)
+logger.info("Logger setup")
+
+db = MySQLdb.connect(host='192.168.5.22',
+                        user='commserver',
+                        passwd='commserver',
+                        db='CollabCommDB')
+
+def get_userdata(uid):
+    d = {}
+    Q = "select sessionid, isActive from Confroom where initiator={0}".format(uid)
+    result = Query(Q).execute()
+    if result != None:
+        d = dict([(r[0],r[1]) for r in result])
+    return d;
+
+class Query:
+    def __init__(self, qstr):
+        self.rawquery = qstr
+    def execute(self):
+        cur = db.cursor()
+        qret = cur.execute(self.rawquery)
+        if qret == 0:
+            result = None
+        else:
+            result = cur.fetchall()
+        return result
--- translet_srv.py
+++ translet_srv.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import json, request, Response
 import MySQLdb
-import json
 import logging
+from app_globals import *
+from db_handler import Query, get_userdata
 
-LOGDIR='/var/log/translet/'
 logging.basicConfig(format="%(asctime)s %(funcName)s %(message)s",
                      filename=LOGDIR+"{0}.log".format(__name__),
                      level='DEBUG')
@@ -12,31 +12,34 @@ logger = logging.getLogger(__name__)
 logger.info("Logger setup")
 srvapp = Flask(__name__)
 
-db = MySQLdb.connect(host='192.168.5.22',
-                        user='commserver',
-                        passwd='commserver',
-                        db='CollabCommDB')
+
+@srvapp.errorhandler(500)
+def internal_error(e):
+    msg = {
+            'staus':500,
+            'message':'Internal server error:{0}'.format(type(e))
+    }
+    resp = jsonify(msg)
+    resp.status_code = 500
+    return resp
+
 
 @srvapp.route("/test/")
 def index():
     logger.info("Test error message")
     return "Hello, world. You're at the polls index."
 
-@srvapp.route("/auth/", methods = ['POST', 'GET'])
+@srvapp.route("/auth/", methods = ['GET'])
 def auth():
     #Validate uname/password or email/password
         #If not valid return 401 status with authorization failed
     try:
         if request.headers['Content-Type'] == 'text/plain':
             requestData = request.args
-            logger.debug(repr(request))
         elif request.headers['Content-Type'] == 'Application/json':
             requestData = json.loads(request.data)
         else:
             return Response('Invalid', status=401)
-        logger.debug("data:"+repr(requestData))
-        status = 200
-        cur = db.cursor()
         user = ''
         if 'email' in requestData:
             logger.debug("email:"+requestData['email']+"Pwd:"+requestData['password'])
@@ -45,31 +48,38 @@ def auth():
             logger.debug("uname:"+requestData['uname']+"Pwd:"+requestData['password'])
             user = "uname='"+requestData['uname']+"'"
         qry = "select uid from Users where "+user+" AND password='"+requestData['password']+"'"
-        logger.debug("Executing:"+qry)
         resp = ''
-        qret = cur.execute(qry)
-        if qret == 0:
-            resp = __name__+':User/password mismatch.'
-            status = 401
+        qret = Query(qry).execute()
+        if qret == None:
+            msg = {
+                    'status':404,
+                    'message':__name__+':User/password mismatch.'
+            }
+            resp = jsonify(msg)
+            resp.status_code = 404
         else:
-            resp = str(cur.fetchone()[0])
+            msg = {
+                    'uid':qret[0][0]
+            }
+            resp = jsonify(msg)
+            resp.status_code = 200
+        return resp
     except Exception as e:
-        resp = __name__+':Server exception'
         logger.error('{0}:{1}'.format(type(e), str(e)))
-        status = 401
-    return Response(resp, status)
+        return internal_error()
 
 @srvapp.route("/userdata/<uid>", methods = ['GET'])
-def get_userdata(uid):
+def userdata(uid):
     resp = ''
-    status = 201
     try:
-        resp = 'received user:{0}'.format(uid)
+        logger.debug('received user:{0}'.format(uid))
+        udata = get_userdata(uid) 
+        resp = jsonify(udata)
+        resp.status_code = 200
+        return resp
     except Exception as e:
-        resp = __name__+':server exception'
         logger.error('{0}:{1}'.format(type(e), str(e)))
-        status = 401
-    return Response(resp, status)
+        return internal_error(e)
 
 if __name__ == "main":
     srvapp.run(host='192.168.5.22')
